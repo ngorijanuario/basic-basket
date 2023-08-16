93576231ee53ce50d56d8be6965da530e0497df5
Ngori Januário <ngorijanuario@gmail.com>
2023-08-16 23:09:28 +0100

    welcome screen

--- basic basket/ContentView.swift
+++ basic basket/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            componentes
+                .padding(.bottom, 10)
+            
+            entradas
+                .padding(.top, 60)
+            
+            
+            
         }
-        .padding()
+       
     }
 }
 
@@ -23,4 +28,57 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+extension ContentView{
+    
+    private var componentes: some View{
+        VStack(spacing: 25){
+            Image("imagem")
+                .resizable()
+                .frame(width: 300, height: 250)
+        }
+    }
+    
+    private var entradas: some View{
+        VStack(spacing: 15) {
+            Text("Shopping for the basics has never been easier.")
+                .font(.title)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color(red: 0.255, green: 0.215, blue: 0.0))
+            
+            Text("From the Cart to Your Home: Buy the Basics Instantly.")
+                .foregroundColor(.secondary)
+                .fontWeight(.light)
+                .multilineTextAlignment(.center)
+            
+            Button(
+                   action: {
+                       print("try it now")
+                   },
+                   label: {
+                        Text("try it now")
+                            .fontWeight(.bold)
+                            .frame(width: 250,height: 50)
+                            .foregroundColor(.white)
+                            .background(Color(red: 0.255, green: 0.215, blue: 0.0))
+                            .clipShape(Capsule())
+                }
+            )
+            
+            HStack(alignment: .center){
+                Text("I accept the")
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+                Link("privacy conditions",
+                     destination: URL(string: "https://www.bicuar.ao/privacidade")!)
+                .foregroundColor(Color(red: 0.255, green: 0.215, blue: 0.0))
+                
+            }
+            
+        }
+    }
+    
 }
