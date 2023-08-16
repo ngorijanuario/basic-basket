8e6ed5d937d56886ce4a8f56f544646b6d620146
Ngori Januário <ngorijanuario@gmail.com>
2023-08-16 22:05:28 +0100

    frist version

---
+++ basic basket/basic_basketApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_basketApp.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct basic_basketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
