99e97b01ffbed0d87ac140a883bb39813f982d9c
Ngori Januário <ngorijanuario@gmail.com>
2023-08-19 11:45:23 +0100

    Improving the look

--- basic basket/views/s1.swift
+++ basic basket/views/s1.swift
@@ -7,343 +7,124 @@
 
 import SwiftUI
 
-struct s1: View {
-    var body: some View {
-        
-        
-        VStack {
-            HStack{
-                Text("Cesta Basíca")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                    
-                Spacer()
-            }
-            ScrollView(.horizontal){
-                HStack{
-                    Button(
-                           action: {
-                               print("All features")
-                           },
-                           label: {
-                                Text("All features")
-                                    .fontWeight(.bold)
-                                    .frame(width: 150,height: 50)
-                                    .foregroundColor(.white)
-                                    .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0))
-                                    .clipShape(Capsule())
-                        }
-                    )
-                    Button(
-                           action: {
-                               print("Grains and Cereals")
-                           },
-                           label: {
-                                Text("Grains and Cereals")
-                                    .fontWeight(.bold)
-                                    .frame(width: 150,height: 50)
-                                    .foregroundColor(.white)
-                                    .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                    .clipShape(Capsule())
-                        }
-                    )
-                    
-                    Button(
-                           action: {
-                               print("Meat and fish")
-                           },
-                           label: {
-                                Text("Meat and fish")
-                                    .fontWeight(.bold)
-                                    .frame(width: 150,height: 50)
-                                    .foregroundColor(.white)
-                                    .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                    .clipShape(Capsule())
-                        }
-                    )
-                    
-                    Button(
-                           action: {
-                               print("Vegetables and Greens")
-                           },
-                           label: {
-                                Text("Vegetables and Greens")
-                                    .fontWeight(.bold)
-                                    .frame(width: 150,height: 50)
-                                    .foregroundColor(.white)
-                                    .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                    .clipShape(Capsule())
-                        }
-                    )
-                    
-                }
-                
-                
-            }
-            
-            ScrollView(.vertical){
-                HStack{
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                        
-                    
-                    VStack{
-                        
-                        Image("fuba")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("300,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Corn meal")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                }
-                
-                HStack{
-                    VStack{
-                        
-                        Image("frango")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("1.500,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Chicken")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                }
-                
-                HStack{
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                }
-                
-                HStack{
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                    VStack{
-                        
-                        Image("açucar")
-                            .resizable()
-                            .frame(width: 135, height: 135)
-                        Text("600,00kz")
-                            .foregroundColor(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
-                        Text("Sugar")
-                            .font(.headline)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color.gray)
-                            .multilineTextAlignment(.center)
-                            .frame(width: 135.0)
-                        
-                        Button(
-                               action: {
-                                   print("Add to Cart")
-                               },
-                               label: {
-                                    Text("Add to Cart")
-                                        .fontWeight(.bold)
-                                        .frame(width: 150,height: 50)
-                                        .foregroundColor(.white)
-                                        .background(Color(hue: 0.115, saturation: 0.979, brightness: 1.0, opacity: 0.379))
-                                        .clipShape(Capsule())
-                            }
-                        )
-                        
-                    }.padding().border(Color(hue: 0.124, saturation: 1.0, brightness: 1.0), width: 2)
-                    
-                }
-            }
-            
-            
-           
-        }
-        .padding()
+
+struct Product: Identifiable {
+    let id = UUID()
+    let name: String
+    let category: String
+    let imageName: String
+    let price: Double
+}
+
+class CartItem: Identifiable {
+    let id = UUID()
+    let product: Product
+    var quantity: Int
+    
+    init(product: Product, quantity: Int) {
+        self.product = product
+        self.quantity = quantity
     }
 }
 
+class Cart: ObservableObject {
+    @Published var items: [CartItem] = []
+    
+    func addItem(_ product: Product) {
+        if let existingItemIndex = items.firstIndex(where: { $0.product.id == product.id }) {
+            items[existingItemIndex].quantity += 1
+        } else {
+            items.append(CartItem(product: product, quantity: 1))
+        }
+    }
+}
+
+struct s1: View {
+    @State private var searchText = ""
+    @State private var selectedCategory = "All features"
+    @StateObject private var cart = Cart()
+    
+    let categories = ["All features", "Vegetables", "Meat", "fish"]
+    
+    let products: [Product] = [
+        Product(name: "Sugar", category: "All features", imageName: "açucar", price: 1.0),
+        Product(name: "Chicken", category: "Meat", imageName: "frango", price: 2.5),
+        Product(name: "Corn meal", category: "All features", imageName: "fuba", price: 0.8),
+        // ... adicione mais produtos aqui
+    ]
+    
+    var filteredProducts: [Product] {
+        if searchText.isEmpty && selectedCategory == "All features" {
+            return products
+        } else if selectedCategory == "All features" {
+            return products.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        } else {
+            return products.filter { $0.name.localizedCaseInsensitiveContains(searchText) && $0.category == selectedCategory }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                Picker("filter by category", selection: $selectedCategory) {
+                    ForEach(categories, id: \.self) { category in
+                        Text(category).tag(category)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.horizontal)
+                
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 16) {
+                        ForEach(filteredProducts) { product in
+                            ProductCardView(product: product)
+                                .environmentObject(cart)
+                        }
+                    }
+                    .padding(.horizontal)
+                }
+            }
+            .navigationTitle("Marketplace")
+        }
+    }
+}
+
+struct ProductCardView: View {
+    let product: Product
+    @EnvironmentObject var cart: Cart
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(product.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 120)
+            
+            Text(product.name)
+                .font(.headline)
+            
+            Text(String(format: "AO%.2f ", product.price))
+                .font(.subheadline)
+                .foregroundColor(.green)
+            
+            Button(action: {
+                cart.addItem(product)
+            }) {
+                Text("+")
+                    .padding(8)
+                    .frame(width: 100,height: 50)
+                    .background(Color(hue: 0.115, saturation: 1.0, brightness: 1.0))
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 3)
+    }
+}
 struct s1_Previews: PreviewProvider {
     static var previews: some View {
         s1()
