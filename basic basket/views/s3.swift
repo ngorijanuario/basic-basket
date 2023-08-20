5c2095c44df64c0084cc57cc1756a9444024a23b
Ngori Januário <ngorijanuario@gmail.com>
2023-08-20 13:11:32 +0100

    removing the unnecessary

--- basic basket/views/s3.swift
+++ basic basket/views/s3.swift
@@ -7,37 +7,6 @@
 
 import SwiftUI
 
-import SwiftUI
-
-class Item: Identifiable {
-    let id: Int
-    let name: String
-    let price: Double
-    
-    init(id: Int, name: String, price: Double) {
-        self.id = id
-        self.name = name
-        self.price = price
-    }
-}
-
-class CartViewModel: ObservableObject {
-    @Published var cartItems: [Item] = []
-    
-    func addToCart(_ item: Item) {
-        cartItems.append(item)
-    }
-    
-    func removeFromCart(_ item: Item) {
-        cartItems.removeAll { $0.id == item.id }
-    }
-    
-    var totalPrice: Double {
-        cartItems.reduce(0) { $0 + $1.price }
-    }
-}
-
-
 struct CartView: View {
     @ObservedObject var cartViewModel: CartViewModel
     
@@ -71,11 +40,6 @@ struct CartView: View {
 }
 
 struct s3: View {
-    let items: [Item] = [
-        Item(id: 1, name: "Item A", price: 10.0),
-        Item(id: 2, name: "Item B", price: 15.0),
-        Item(id: 3, name: "Item C", price: 20.0)
-    ]
     
     @StateObject private var cartViewModel = CartViewModel()
     
