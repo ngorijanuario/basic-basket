f5cb14071d242e9bdd3788b8716ee0e0c4680c04
Ngori Januário <ngorijanuario@gmail.com>
2023-08-20 13:06:13 +0100

    category picker

--- basic basket/views/s1.swift
+++ basic basket/views/s1.swift
@@ -7,70 +7,32 @@
 
 import SwiftUI
 
-
-struct Product: Identifiable {
-    let id = UUID()
-    let name: String
-    let category: String
-    let imageName: String
-    let price: Double
-}
-
-class CartItem: Identifiable {
-    let id = UUID()
-    let product: Product
-    var quantity: Int
-    
-    init(product: Product, quantity: Int) {
-        self.product = product
-        self.quantity = quantity
-    }
-}
-
-class Cart: ObservableObject {
-    @Published var items: [CartItem] = []
-    
-    func addItem(_ product: Product) {
-        if let existingItemIndex = items.firstIndex(where: { $0.product.id == product.id }) {
-            items[existingItemIndex].quantity += 1
-        } else {
-            items.append(CartItem(product: product, quantity: 1))
-        }
-    }
-}
-
 struct s1: View {
-    
-    
     
     @State private var searchText = ""
     @State private var selectedCategory = "All features"
-    @StateObject private var cart = Cart()
+    var cart = CartViewModel()
     
-    let categories = ["All features", "Vegetables", "Meat", "fish"]
+    let importproducts = dataservice()
+    let importcategories = dataservice()
     
-    let products: [Product] = [
-        Product(name: "Sugar", category: "All features", imageName: "açucar", price: 600.00),
-        Product(name: "Chicken", category: "Meat", imageName: "frango", price: 800.00),
-        Product(name: "Corn meal", category: "All features", imageName: "fuba", price: 250.08),
-        // ... adicione mais produtos aqui
-    ]
     
     var filteredProducts: [Product] {
         if searchText.isEmpty && selectedCategory == "All features" {
-            return products
+            return importproducts.products
         } else if selectedCategory == "All features" {
-            return products.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+            return importproducts.products.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
         } else {
-            return products.filter { $0.name.localizedCaseInsensitiveContains(searchText) && $0.category == selectedCategory }
+            return importproducts.products.filter { $0.category == selectedCategory }
         }
     }
     
     var body: some View {
+        
         NavigationView {
             VStack {
                 Picker("filter by category", selection: $selectedCategory) {
-                    ForEach(categories, id: \.self) { category in
+                    ForEach(importcategories.categories, id: \.self) { category in
                         Text(category).tag(category)
                     }
                 }
@@ -94,7 +56,7 @@ struct s1: View {
 
 struct ProductCardView: View {
     let product: Product
-    @EnvironmentObject var cart: Cart
+    //@EnvironmentObject var cart: CartViewModel()
     
     var body: some View {
         VStack(spacing: 8) {
@@ -111,7 +73,7 @@ struct ProductCardView: View {
                 .foregroundColor(.green)
             
             Button(action: {
-                cart.addItem(product)
+                //
             }) {
                 Text("+")
                     .padding(8)
