7a689d0ea3a4c385c08a89ce1aaef158aa114175
Ngori Januário <ngorijanuario@gmail.com>
2023-08-25 12:16:30 +0100

    creat cardProduct

---
+++ basic basket/viewsModel/cardProduct.swift
@@ -0,0 +1,25 @@
+//
+//  cardProduct.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 25/08/23.
+//
+
+import SwiftUI
+
+struct cardProduct: View {
+    
+    
+    var body: some View {
+        VStack() {
+            Text("cardProduct")
+        }
+        .padding()
+    }
+}
+
+struct cardProduct_Previews: PreviewProvider {
+    static var previews: some View {
+        cardProduct()
+    }
+}
