2e7b4bdd6264d6453d32dc743d399dddab7232cf
Ngori Januário <ngorijanuario@gmail.com>
2023-08-27 12:47:14 +0100

    FAQ screen

---
+++ basic basket/views/FAQ.swift
@@ -0,0 +1,19 @@
+//
+//  FAQ.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 27/08/23.
+//
+
+import SwiftUI
+
+struct FAQ: View {
+    var body: some View {
+        Text("FAQ")
+    }
+}
+struct FAQ_Previews: PreviewProvider {
+    static var previews: some View {
+        FAQ()
+    }
+}
