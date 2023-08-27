b13b1e3a1fd1d6840e5934962945609f4539134b
Ngori Januário <ngorijanuario@gmail.com>
2023-08-27 12:47:36 +0100

    InviteFriends screen

---
+++ basic basket/views/InviteFriends.swift
@@ -0,0 +1,19 @@
+//
+//  InviteFriends.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 27/08/23.
+//
+
+import SwiftUI
+
+struct InviteFriends: View {
+    var body: some View {
+        Text("Invite Friends")
+    }
+}
+struct InviteFriends_Previews: PreviewProvider {
+    static var previews: some View {
+        InviteFriends()
+    }
+}
