a50b7e73a97f225f85161da1b80659723f22c017
Ngori Januário <ngorijanuario@gmail.com>
2023-08-27 12:46:44 +0100

    ContactSupport screen

---
+++ basic basket/views/ContactSupport.swift
@@ -0,0 +1,20 @@
+//
+//  ContactSupport.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 27/08/23.
+//
+
+import SwiftUI
+
+struct ContactSupport: View {
+    var body: some View {
+        Text("Contact Support")
+    }
+}
+
+struct ContactSupport_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactSupport()
+    }
+}
