b46df8bde8c019587ca3c35eef9fa06dbc04cb50
Ngori Januário <ngorijanuario@gmail.com>
2023-08-27 12:46:16 +0100

    Privacy screen

---
+++ basic basket/views/Privacy.swift
@@ -0,0 +1,20 @@
+//
+//  Privacy.swift
+//  basic basket
+//
+//  Created by Ngori Januario on 27/08/23.
+//
+
+import SwiftUI
+
+struct Privacy: View {
+    var body: some View {
+        Text("Privacy")
+    }
+}
+
+struct Privacy_Previews: PreviewProvider {
+    static var previews: some View {
+        Privacy()
+    }
+}
